b7e96c52b8dcb319d4e137bec5621dae3b9ba26e
Karl <cipherus@users.noreply.github.com>
2017-10-29 20:27:41 +0100

    changed show option back to display version

--- isThatAUnicorn.js
+++ isThatAUnicorn.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+                 
+                   
 
                   
                   var winCounter = 0;
@@ -7,19 +9,23 @@ $(document).ready(function() {
                   var orientations = ["135", "0", "225", "90", "180", "45", "270", "315"];
                   var accuracy = "20"; //in degrees
                   var alpha = 0;
+                  $("#rewardarea").hide();
+                  $("#failarea").hide();
+                  document.getElementById('rewardarea').style.display = "none";
+                  document.getElementById('failarea').style.display = "none";
 
-              
-                 
-                  document.getElementById('rewardarea').style.visibility = "hidden";
-                  document.getElementById('failarea').style.visibility = "visible";
-                  /*if(window.DeviceOrientationEvent) {
+               
+              if(window.DeviceOrientationEvent) {
                                      window.addEventListener('deviceorientation', function(event) {
                                                 alpha = Math.floor(event.alpha); //Round down to integer
-                                                    }, false );
-                  }
+                                                    }, false);
+                                   }
+                  
+                  
                   
                   $("#button").click(function() {
-
+                                   
+                                   
 
                                       if (orientationValue==1 && (360 - accuracy < alpha || alpha < accuracy) ||
                                        ((orientations[orientationValue] - accuracy) < alpha && alpha < (orientations[orientationValue] + accuracy) ) ) { //special rule for north because of overflow
@@ -27,13 +33,20 @@ $(document).ready(function() {
                                        winCounter++;
                                        $("#mainarea").hide();
                                        $("#rewardarea").show();
+                                       document.getElementById('mainarea').style.display = "none";
+                                       document.getElementById('rewardarea').style.display = "block";
+                                       document.getElementById('failarea').style.display = "none";
 
                                       }
                                       else {
                                         winCounter = 0;
                                        $("#mainarea").hide();
                                        $("#failarea").show();
+                                       document.getElementById('mainarea').style.display = "none";
+                                       document.getElementById('rewardarea').style.display = "none";
+                                       document.getElementById('failarea').style.display = "block";
                                       }
+
                                       window.setTimeout(function() {
                                       $("#winCounter").text(winCounter); //Set wincounter
                               
@@ -44,14 +57,22 @@ $(document).ready(function() {
                                       $("#rewardarea").hide();
                                       $("#failarea").hide();
                                       $("#mainarea").show();
+                                       document.getElementById('mainarea').style.display = "block";
+                                       document.getElementById('rewardarea').style.display = "none";
+                                       document.getElementById('failarea').style.display = "none";
+
+
 
                                       }, 2000);
-                                    
 
-                                 
-                                    
-                                    
+
+                                      
+
+                                     
+
+                              
+       
                     });
-                    */
+                    
                     
 });
